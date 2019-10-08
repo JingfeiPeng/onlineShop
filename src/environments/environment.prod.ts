f2dc0c34044eac1ad9edac8b4396d8be1020a6dc
Jeff Peng <Jeff.Peng@capco.com>
2019-10-08 19:26:37 -0400

    initisl setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBQIretCjYYgz1qzVrxTOvLp7tclE3rE8w",
+    authDomain: "oshop-121f5.firebaseapp.com",
+    databaseURL: "https://oshop-121f5.firebaseio.com",
+    projectId: "oshop-121f5",
+    storageBucket: "",
+    messagingSenderId: "119563058411",
+    appId: "1:119563058411:web:1b8eb8ba54080ea5f2d2ae"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBQIretCjYYgz1qzVrxTOvLp7tclE3rE8w",
+    authDomain: "oshop-121f5.firebaseapp.com",
+    databaseURL: "https://oshop-121f5.firebaseio.com",
+    projectId: "oshop-121f5",
+    storageBucket: "",
+    messagingSenderId: "119563058411",
+    appId: "1:119563058411:web:1b8eb8ba54080ea5f2d2ae"
+  }
 };
 
 /*
