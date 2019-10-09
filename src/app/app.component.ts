255881cc9d7b4a2be1f3a8a8e06049d3f78cc58d
Jeff Peng <Jeff.Peng@capco.com>
2019-10-09 01:29:18 -0400

    JP: added authentication with google and route guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router'
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'oshop';
+export class AppComponent{
+  constructor(private auth: AuthService,
+              private router: Router, 
+              private userService: UserService){
+    auth.user$.subscribe(user =>{
+      if (user){
+        userService.save(user);
+
+        let returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    })
+  }
 }
+
